6d495d9410b10c2dd66c9e6f68499eefdf4bcd4f
Marco L <mfl-git@bitblox.de>
2021-04-09 16:33:28 +0200

    Initial commit of a working version. Tested with '1.197.181 default'.

---
+++ MainLogicComponent.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections.Generic;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+using VRageMath;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI;
+using MyDetectedEntityType = Sandbox.ModAPI.Ingame.MyDetectedEntityType;
+using MyDetectedEntityInfo = Sandbox.ModAPI.Ingame.MyDetectedEntityInfo;
+
+namespace SpaceEngineers.Mod.TeleportingSensor
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_SensorBlock), true)]
+    public class LogicComponent : MyGameLogicComponent
+    {
+        public static string PROPERTY_PREFIX = "teleporter.";
+        public static string PROPERTY_KEY_NAME = $"{PROPERTY_PREFIX}name";
+        public static string PROPERTY_KEY_TARGET = $"{PROPERTY_PREFIX}target";
+        public static string PROPERTY_KEY_OFFSET = $"{PROPERTY_PREFIX}offset";
+        public static string  PROPERTY_KEY_DEBUG = $"{PROPERTY_PREFIX}debug";
+
+        public static Dictionary<String, LogicComponent> TeleporterNetwork = new Dictionary<string, LogicComponent>();
+        public bool Valid = false;
+
+        public string Name;
+        public string Target;
+        public Vector3D Offset = new Vector3D(0);
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            Init(true);
+            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_100TH_FRAME;
+        }
+
+        public override void UpdateBeforeSimulation100()
+        {
+            Init();
+
+            IMySensorBlock sensorBlock = Entity as IMySensorBlock;
+
+            if (Target != null && sensorBlock.IsActive)
+            {
+                PrintDebugMessage($"'{sensorBlock.DisplayNameText}' is an active teleporter to '{Target}'.");
+                List<MyDetectedEntityInfo> detectedEntityInfos = new List<MyDetectedEntityInfo>();
+                sensorBlock.DetectedEntities(detectedEntityInfos);
+                List<IMyPlayer> players = new List<IMyPlayer>();
+
+                foreach (MyDetectedEntityInfo entityInfo in detectedEntityInfos)
+                {
+                    if (entityInfo.Type == MyDetectedEntityType.CharacterHuman) {
+                        MyAPIGateway.Players.GetPlayers(players, p => p.Character.EntityId == entityInfo.EntityId);
+                        IMyPlayer player = players[0];
+
+                        PrintDebugMessage($"'{sensorBlock.DisplayNameText}' detected '{player.DisplayName}' and will teleport it to '{TeleporterNetwork[Target].Name}'.");
+                        Teleport(player.Character, TeleporterNetwork[Target]);
+                    }
+                }
+            }
+        }
+
+        private void Init(bool firstTime = false)
+        {
+            IMySensorBlock sensorBlock = Entity as IMySensorBlock;
+
+            if (sensorBlock.CustomData.Contains($"{PROPERTY_KEY_NAME}="))
+            {
+                Dictionary<string, string> props = ReadProperties(sensorBlock.CustomData);
+
+                if (!props.ContainsKey(PROPERTY_KEY_NAME)) {
+                    PrintDebugMessage($"'{sensorBlock.DisplayNameText}' has '{PROPERTY_KEY_NAME}=' in Custom Data, but it can not be used/read.");
+                    return;
+                }
+
+                // Idea: Check for changed name and update the teleporter-network automatically ?!
+                Name = props[PROPERTY_KEY_NAME];
+
+                if (props.ContainsKey(PROPERTY_KEY_OFFSET))
+                {
+                    string[] coordStrs = props[PROPERTY_KEY_OFFSET].Split(',');
+                    try
+                    {
+                        Offset = ParseVector3D(coordStrs);
+                    }
+                    catch (Exception e)
+                    {
+                        PrintErrorMessage($"While reading '{PROPERTY_KEY_OFFSET}' from '{sensorBlock.DisplayNameText}': {e.Message}");
+                    }
+                }
+
+                try
+                {
+                    Register(this);
+                }
+                catch (Exception e)
+                {
+                    PrintErrorMessage(e.Message);
+                    return;
+                }
+
+                if (!props.ContainsKey(PROPERTY_KEY_TARGET))
+                {
+                    // One-Way teleporter. This might be a target only.
+                    //PrintWarningMessage($"'{PROPERTY_KEY_TARGET}' not set in Custom Data at '{sensorBlock.DisplayNameText}'. It might be a target only.");
+                    return;
+                }
+
+                Target = props[PROPERTY_KEY_TARGET];
+
+                if (!TeleporterNetwork.ContainsKey(Target) && !firstTime) {
+                    PrintErrorMessage($"No teleporter found with the name '{Target}'!");
+                }
+            }
+        }
+
+        private static void Teleport(IMyEntity entity, LogicComponent target)
+        {
+            Vector3D newPosition = target.Entity.GetPosition() + target.Entity.WorldMatrix.Forward * 3f + target.Offset;
+            //entity.SetPosition(newPosition);
+            MatrixD newPosMatrix = MatrixD.CreateWorld(newPosition, target.Entity.WorldMatrix.Forward, target.Entity.WorldMatrix.Up);
+            entity.Teleport(newPosMatrix);
+        }
+
+        private static void Register(LogicComponent teleporter)
+        {
+            if (!TeleporterNetwork.ContainsKey(teleporter.Name))
+            {
+                TeleporterNetwork.Add(teleporter.Name, teleporter);
+            }
+            else if (TeleporterNetwork[teleporter.Name] != teleporter)
+            {
+                throw new Exception($"There is already a teleporter registered with the name '{teleporter.Name}'!");
+            }
+        }
+
+        private static Dictionary<string, string> ReadProperties(string source) {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            string[] lines = source.Split('\n');
+            string[] pair;
+
+            foreach (var line in lines)
+            {
+                pair = line.Split(new char[1] { '=' }, 2);
+                if (pair.Length == 2) {
+                    result.Add(pair[0].ToLower(), pair[1]);
+                } else {
+                    result.Add(pair[0].ToLower(), "");
+                }
+            }
+
+            return result;
+        }
+
+        private static float ParseFloat(string src, string errDetails)
+        {
+            float result;
+
+            if (!float.TryParse(src, out result))
+            {
+                throw new Exception(errDetails);
+            }
+
+            return result;
+        }
+
+        private static Vector3D ParseVector3D(string[] coords)
+        {
+            // TODO: Be sure it is also handling 0, 1 or 2 coordinates.
+            float x = ParseFloat(coords[0].Trim(), "x-coordinate is not a number.");
+            float y = ParseFloat(coords[1].Trim(), "y-coordinate is not a number.");
+            float z = ParseFloat(coords[2].Trim(), "z-coordinate is not a number.");
+
+            return new Vector3D(x, y, z);
+        }
+
+        private static void PrintMessage(string text, int disappearTimeMs = 3000, string fontColor = MyFontEnum.Green)
+        {
+            MyAPIGateway.Utilities.ShowNotification(text, disappearTimeMs, fontColor);
+        }
+
+        private static void PrintErrorMessage(string text, int disappearTimeMs = 3000, string fontColor = MyFontEnum.Red)
+        {
+            PrintMessage($"ERROR: {text}", disappearTimeMs, fontColor);
+        }
+
+        private static void PrintWarningMessage(string text, int disappearTimeMs = 3000, string fontColor = MyFontEnum.DarkBlue)
+        {
+            PrintMessage($"WARNING: {text}", disappearTimeMs, fontColor);
+        }
+
+        private void PrintDebugMessage(string text, int disappearTimeMs = 3000, string fontColor = MyFontEnum.Debug)
+        {
+            if ((Entity as IMyTerminalBlock).CustomData.Contains($"{PROPERTY_KEY_DEBUG}=true")) {
+                PrintMessage($"DEBUG: {text}", disappearTimeMs, fontColor);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,118 @@
+![Thumbnail](thumb.png)
+
+# Short description
+An mod for Space Engineers that allows using sensors for teleportation.
+
+# Usage
+
+1. Enable the mod `TeleportingSensor` in the savegame settings.
+1. Create two sensors somewhere at two different places.
+1. Adjust the both sensor ranges like:
+
+        Left extent  : 0.1 m
+        Right extent : 0.1 m
+        Bottom extent: 0.1 m
+        Top extent   : 0.1 m
+        Back extent  : 0.1 m
+        Front extent : 1.0 m
+
+1. Edit the Custom Data of the first one:
+
+        teleporter.name=Teleporter 1
+        teleporter.target=Teleporter 2
+
+1. Edit the Custom Data of the second one:
+
+        teleporter.name=Teleporter 2
+        teleporter.target=Teleporter 1
+
+1. Reach 1.0 meters of range to the front of one sensor.
+1. *Bzzzzt!* Find your self 3.0 meters away from the other sensor.
+
+# Building simple teleporter gates
+
+1. Build two gates like on the thumbnail:
+
+        2x steel blocks vertically on the left side
+        1x block space
+        2x steel blocks vertically on the right side
+        1x rounded corner block on the top of each pillar pointing to the center
+        1x steel block between the rounded corner blocks
+        1x sensor below the center steel block
+        1x light on the inner side of the center space
+        2x LCDs on the center block showing the destination
+
+1. Adjust the sensors ranges:
+
+        Left extent  : 1.0 m
+        Right extent : 1.0 m
+        Bottom extent: 1.0 m
+        Top extent   : 1.0 m
+        Back extent  : 0.1 m
+        Front extent : 4.5 m
+
+1. Edit the Custom Data of the first one:
+
+        teleporter.name=Teleporter 3
+        teleporter.target=Teleporter 4
+
+1. Edit the Custom Data of the second one:
+
+        teleporter.name=Teleporter 4
+        teleporter.target=Teleporter 3
+
+1. Step into the gate.
+1. *Bzzzzt!* Find your self in the other teleporter gate.
+1. Step out quickly, or you will get into a teleporter loop. *Improve this with `teleporter.offset`!*
+
+# Custom Data (Properties)
+
+## Properties on the sensors
+
+The sensors which shall function as a teleporter need to be configured via its `Custom Data`.
+
+### `teleporter.name` (string)
+
+This `string`-property will define a unique(!) name for the teleporter to identify and use it on another teleporting sensor.
+
+**IMPORTANT:** If this is not given, the sensor will not be identified as a teleporting sensor!
+
+#### Example
+
+`teleporter.name=Teleporter 1`
+
+### `teleporter.target` (string)
+
+This `string`-property will define the name of a teleporting sensor to teleport to. This has to be set on the target sensors Custom Data via `teleporter.name`.
+
+This is optional. If not given, the sensor will be a `receive-only` target.
+
+#### Example
+
+`teleporter.target=Teleporter 2`
+
+### `teleporter.offset` (float,float,float)
+
+This property is a comma-separated vector/list of `float`-values with 3 entries defining an offset 3 meters in front of the sensor to teleport the player to.
+
+This is very useful to adjust the teleportation spots to break teleporter loops or to place the player on a specific block away from the sensor.
+
+**IMPORTANT:** There is no limitation in this offset, so one could use it to teleport the player where ever one likes. To other planets, asteroids or even into empty space. **So be very sensitive with this!**
+
+The first entry will define the offset in meters on the x-axis, the second the offset on the y-axis and the third the offset on the z-axis.
+
+#### Example
+
+`teleporter.offset=0.0,5.0,0.0`
+
+The result of the example would be: `sensor.y + sensor.front.y * 3.0 m + 5.0 m`
+
+That's between 5 and 8 meters away from the sensors front side on the y-axis (depending on the sensors direction / alignment with the y-axis).
+
+# Tested
+
+with Space Engineers Version `1.197.181 default` (no other mods active)
+
+# Source code
+
+[https://github.com/mflink3012/spaceengineers-mod-teleportingsensors](https://github.com/mflink3012/spaceengineers-mod-teleportingsensors)
